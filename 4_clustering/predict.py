7144fac40daa2c31ade8be3c3ee807c3d4a62da1
wandora58 <nakajima@m-icl.ist.hokudai.ac.jp>
2020-01-08 06:02:06 +0900

    4_clustering

---
+++ 4_clustering/predict.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn import linear_model
+import sklearn.model_selection
+
+# データ読み込み(入会が 2018/4 以降を対象とする)
+train_data = pd.read_csv('train_data.csv')
+train_data['start_date'] = pd.to_datetime(train_data['start_date'])
+train_data = train_data.loc[train_data['start_date']>=pd.to_datetime('20180401')]
+
+# 訓練データと教師データに分ける
+X = train_data[['count_0', 'count_1', 'count_2', 'count_3', 'count_4', 'count_5', 'period']]
+y = train_data['count_pred']
+
+# 検証データに分割
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y)
+
+# モデル定義
+model = linear_model.LinearRegression()
+
+# モデル訓練
+model.fit(X_train, y_train)
+
+# 訓練精度と検証精度
+print(model.score(X_train, y_train))
+print(model.score(X_test, y_test))
+
+# 回帰モデルの説明変数ごとの寄与係数確認
+coef = pd.DataFrame({'feature_names':X.columns, 'coefficient':model.coef_})
+print(coef)
+
---
+++ 4_clustering/read_data.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+customer = pd.read_csv('dump_data.csv')
+print(customer.head())
